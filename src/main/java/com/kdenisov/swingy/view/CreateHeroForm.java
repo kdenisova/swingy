0d5320af79ad089c8fe654c8741dd4b56c3e7a3f
Kseniia Denisova <angrynimfa@Kseniias-MacBook-Air.local>
2020-02-24 14:59:40 -0800

    Added link between combo boxes and values

---
+++ src/main/java/com/kdenisov/swingy/view/CreateHeroForm.java
@@ -0,0 +1,18 @@
+package com.kdenisov.swingy.view;
+
+import javax.swing.*;
+
+public class CreateHeroForm {
+    private JLabel nameLabel;
+    private JTextField nameField;
+    private JLabel heroClassLabel;
+    private JComboBox heroClassBox;
+    private JLabel atrefactLabel;
+    private JComboBox artefactBox;
+    private JLabel attackLabel;
+    private JTextField attackField;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
--- src/main/java/com/kdenisov/swingy/view/CreateHeroFrame.java
+++ src/main/java/com/kdenisov/swingy/view/CreateHeroFrame.java
@@ -5,104 +5,106 @@ import com.kdenisov.swingy.model.HeroClass;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.text.html.parser.Parser;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
 public class CreateHeroFrame {
-    JFrame frame;
+    private int attack, defense, hitPoints;
+    private JFrame frame;
+    private JComboBox heroClassBox;
+    private JComboBox artefactBox;
+    private JTextField attackField;
+    private JTextField defenseField;
+    private JTextField hitField;
 
     public void createHero() {
         frame = new JFrame("Create a new Hero");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-
-        JButton saveButton = new JButton("Save");
-        //saveButton.addActionListener(new GUIRenderer.NewGameButtonListener());
-
-        JButton cancelButton = new JButton("Cancel");
-        cancelButton.addActionListener(new CancelButtonListener());
+        frame.setLayout(null);
 
         JLabel nameLabel = new JLabel("Name");
-        nameLabel.setSize(100, 30);
-        nameLabel.setLocation(50, 50);
+        nameLabel.setBounds(50, 50, 100, 30);
         frame.add(nameLabel);
 
         JTextField nameField = new JTextField();
-        nameField.setSize(190, 30);
-        nameField.setLocation(150, 50);
+        nameField.setBounds(150, 50, 190, 30);
         frame.add(nameField);
 
-        JLabel classLabel = new JLabel("Hero Class");
-        classLabel.setSize(100, 30);
-        classLabel.setLocation(50, 100);
-        frame.add(classLabel);
+        JLabel heroClassLabel = new JLabel("Hero Class");
+        heroClassLabel.setBounds(50, 100, 100, 30);
+        frame.add(heroClassLabel);
 
-        JComboBox classBox = new JComboBox(HeroClass.values());
-        classBox.setSize(190, 30);
-        classBox.setLocation(150, 100);
-        frame.add(classBox);
+        heroClassBox = new JComboBox(HeroClass.values());
+        heroClassBox.setBounds(150, 100, 190, 30);
+        heroClassBox.setSelectedIndex(-1);
+        heroClassBox.addActionListener(new HeroClassBoxListener());
+        frame.add(heroClassBox);
 
         JLabel artifactLabel = new JLabel("Artefact");
-        artifactLabel.setSize(100, 30);
-        artifactLabel.setLocation(50, 150);
+        artifactLabel.setBounds(50, 150, 100, 30);
         frame.add(artifactLabel);
 
-        JComboBox artefactBox = new JComboBox(Artefact.values());
-        artefactBox.setSize(190, 30);
-        artefactBox.setLocation(150, 150);
+        artefactBox = new JComboBox(Artefact.values());
+        artefactBox.setBounds(150, 150, 190, 30);
+        artefactBox.setSelectedIndex(-1);
+        artefactBox.addActionListener(new ArtefactBoxListener());
         frame.add(artefactBox);
 
-        ImageIcon heroIcon = new ImageIcon("/Users/angrynimfa/Downloads/elf11.jpg");
+        ImageIcon heroIcon = new ImageIcon("/Users/angrynimfa/projects/swingy/src/main/resources/elf4.jpg");//"/Users/angrynimfa/Downloads/elf11.jpg");
         JLabel iconLabel = new JLabel(heroIcon);
-        iconLabel.setSize(200, 200);
-        iconLabel.setLocation(370, 30);
+        iconLabel.setBounds(400, 45, 150, 150);
         frame.add(iconLabel);
 
         JLabel attackLabel = new JLabel("Attack");
-        attackLabel.setSize(100, 30);
-        attackLabel.setLocation(50, 250);
+        attackLabel.setBounds(50, 200, 100, 30);
         frame.add(attackLabel);
 
-        JTextField attackField = new JTextField("0");
-        attackField.setSize(50, 30);
-        attackField.setLocation(100, 250);
+        attackField = new JTextField("0");
+        attackField.setBounds(100, 200, 50, 30);
         attackField.setEnabled(false);
         frame.add(attackField);
 
         JLabel defenseLabel = new JLabel("Defense");
-        defenseLabel.setSize(100, 30);
-        defenseLabel.setLocation(170, 250);
+        defenseLabel.setBounds(170, 200, 100, 30);
         frame.add(defenseLabel);
 
-        JTextField defenseField = new JTextField("0");
-        defenseField.setSize(50, 30);
-        defenseField.setLocation(230, 250);
+        defenseField = new JTextField("0");
+        defenseField.setBounds(230, 200, 50, 30);
         defenseField.setEnabled(false);
         frame.add(defenseField);
 
         JLabel hitLabel = new JLabel("Hit Points");
-        hitLabel.setSize(100, 30);
-        hitLabel.setLocation(300, 250);
+        hitLabel.setBounds(300, 200, 100, 30);
         frame.add(hitLabel);
 
-        JTextField hitField = new JTextField("0");
-        hitField.setSize(50, 30);
-        hitField.setLocation(370, 250);
+        hitField = new JTextField("0");
+        hitField.setBounds(370, 200, 50, 30);
         hitField.setEnabled(false);
         frame.add(hitField);
 
-        JPanel panel = new JPanel();
-        //panel.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-       // gbc.gridwidth = GridBagConstraints.BOTH;
-        //gbc.fill = GridBagConstraints.SOUTH;
-       // gbc.anchor = GridBagConstraints.PAGE_END;
-        panel.add(saveButton);
-        panel.add(cancelButton);
+        JButton saveButton = new JButton("Save");
+        saveButton.setBounds(100, 250, 100, 30);
+        frame.add(saveButton);
 
-        frame.add(panel, BorderLayout.SOUTH);
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.setBounds(250, 250, 100, 30);
+        cancelButton.addActionListener(new CancelButtonListener());
+        frame.add(cancelButton);
+
+//        JPanel panel = new JPanel();
+//        //panel.setLayout(new GridBagLayout());
+//        GridBagConstraints gbc = new GridBagConstraints();
+//       // gbc.gridwidth = GridBagConstraints.BOTH;
+//        //gbc.fill = GridBagConstraints.SOUTH;
+//       // gbc.anchor = GridBagConstraints.PAGE_END;
+//        panel.add(saveButton);
+//        panel.add(cancelButton);
+//
+//        frame.add(panel, BorderLayout.SOUTH);
         frame.setBounds(50, 50,600, 400);
 
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize(); //Set a window on center of screen
@@ -118,6 +120,65 @@ public class CreateHeroFrame {
         public void actionPerformed(ActionEvent e) {
             frame.dispose();
 
+        }
+    }
+
+    class HeroClassBoxListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (artefactBox.getSelectedIndex() >= 0)
+                artefactBox.setSelectedIndex(-1);
+            if (heroClassBox.getSelectedItem().equals(HeroClass.ELF)) {
+                attack = 100;
+                defense = 50;
+                hitPoints = 15;
+                attackField.setText(String.valueOf(attack));
+                defenseField.setText(String.valueOf(defense));
+                hitField.setText(String.valueOf(hitPoints));
+            } else if (heroClassBox.getSelectedItem().equals(HeroClass.DWARF)) {
+                attack = 110;
+                defense = 60;
+                hitPoints = 12;
+                attackField.setText(String.valueOf(attack));
+                defenseField.setText(String.valueOf(defense));
+                hitField.setText(String.valueOf(hitPoints));
+            }
+            else {
+                attack = 90;
+                defense = 30;
+                hitPoints = 10;
+                attackField.setText(String.valueOf(attack));
+                defenseField.setText(String.valueOf(defense));
+                hitField.setText(String.valueOf(hitPoints));
+            }
+        }
+    }
+
+    class ArtefactBoxListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int value;
+
+            if (artefactBox.getSelectedIndex() >= 0) {
+                if (artefactBox.getSelectedItem().equals(Artefact.WEAPON)) {
+                    value = attack + 10;
+                    attackField.setText(String.valueOf(value));
+                    defenseField.setText(String.valueOf(defense));
+                    hitField.setText(String.valueOf(hitPoints));
+                } else if (artefactBox.getSelectedItem().equals(Artefact.ARMOR)) {
+                    value = defense + 10;
+                    attackField.setText(String.valueOf(attack));
+                    defenseField.setText(String.valueOf(value));
+                    hitField.setText(String.valueOf(hitPoints));
+                } else {
+                    value = hitPoints + 10;
+                    attackField.setText(String.valueOf(attack));
+                    defenseField.setText(String.valueOf(defense));
+                    hitField.setText(String.valueOf(value));
+                }
+            }
         }
     }
 }
